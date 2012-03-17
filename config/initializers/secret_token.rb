bd314bf0989fd216b678fce53a73c977bca379ad
Scott Weston <westons@comcast.net>
2012-03-16 22:51:33 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GolfLeague::Application.config.secret_token = '2f2264bcd32d06dd364d5a77a52139234ce3a0107ae655dae57b2995b622c3d44038b25325d39b55ac770bec35a80fea3853205fdf2febe05c8e77b44e00f808'
